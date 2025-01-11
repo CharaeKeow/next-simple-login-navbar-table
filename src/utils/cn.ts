74bbaa1f16c6331abd4dd8b1ba52c353107a5d21
CharaeKeow <charae.ehsin@gmail.com>
2025-01-11 21:57:40 +0800

    feat: add cn helper function

---
+++ src/utils/cn.ts
@@ -0,0 +1,11 @@
+import { clsx, ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+/**
+ * Utility function for combining and managing CSS classes/Tailwind classes.
+ *
+ * Reference: @see {@link https://ui.shadcn.com/docs/installation/manual#add-a-cn-helper | shadcn/ui}
+ */
+export const cn = (...inputs: ClassValue[]) => {
+  return twMerge(clsx(inputs));
+};
