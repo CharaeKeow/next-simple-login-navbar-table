2932a6ba024c3208e122b2a1826929c8d07d8997
CharaeKeow <charae.ehsin@gmail.com>
2025-01-12 18:15:13 +0800

    chore: remove unused server action

--- src/features/auth/actions/handle-submit-username.ts
+++
@@ -1,9 +0,0 @@
-"use server";
-
-export default async function handleSubmitUsername(formData: FormData) {
-  // TODO: Add zod validation
-
-  const username = formData.get("username");
-
-  console.log("Serer received username: ", username);
-}
