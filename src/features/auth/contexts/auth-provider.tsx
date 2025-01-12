9207dbbe00a0c832d4d662c914ccb5fb453abc98
CharaeKeow <charae.ehsin@gmail.com>
2025-01-12 18:51:54 +0800

    feat: add isAuthenticated flag to auth provider

--- src/features/auth/contexts/auth-provider.tsx
+++ src/features/auth/contexts/auth-provider.tsx
@@ -14,10 +14,12 @@ type AuthContextType = {
   secureWord: string | undefined;
   username: string;
   loginStep: LoginStep;
+  isAuthenticated: boolean;
   // setSecureWord: (secureWord: string | undefined) => void;
   setSecureWord: Dispatch<SetStateAction<string | undefined>>;
   setUsername: Dispatch<SetStateAction<string>>;
   setLoginStep: Dispatch<SetStateAction<LoginStep>>;
+  setIsAuthenticated: Dispatch<SetStateAction<boolean>>;
 };
 
 type AuthProviderProps = {
@@ -32,13 +34,19 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
   const [username, setUsername] = useState<AuthContextType["username"]>("");
   const [loginStep, setLoginStep] = useState<LoginStep>("username"); // login step will always start with username
 
+  // Note: In real world, this would be a state provided by library. E.g. if using Auth.js, we can obtain this from `useSession` hook at client
+  // Since this is just a test app, I resorted to just do this in this provider for simplicity's sake
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   const value: AuthContextType = {
     secureWord,
     username,
     loginStep,
+    isAuthenticated,
     setSecureWord,
     setUsername,
     setLoginStep,
+    setIsAuthenticated,
   };
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
