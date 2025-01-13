e13f82f889be0e65670038ad7e10ca9dd76918de
CharaeKeow <charae.ehsin@gmail.com>
2025-01-14 00:34:23 +0800

    fix: remove redirect from login if already logged in

--- src/app/(common)/login/page.tsx
+++ src/app/(common)/login/page.tsx
@@ -1,14 +1,15 @@
 import { AuthFormView } from '@/features/auth/components/auth-form-view/auth-form-view';
-import { getAuthCookie } from '@/utils/cookies';
-import { redirect } from 'next/navigation';
+// import { getAuthCookie } from '@/utils/cookies';
+// import { redirect } from 'next/navigation';
 
 export default async function LoginPage() {
   // Redirect to other page if access login when already logged in. Ideally this can be done in middleware as well
-  const session = await getAuthCookie();
+  // Note: Removed because user can't see the successful login message (as outlined in the requirement)
+  // const session = await getAuthCookie();
 
-  if (session) {
-    redirect('/transaction-history');
-  }
+  // if (session) {
+  //   redirect('/transaction-history');
+  // }
 
   return <AuthFormView />;
 }
